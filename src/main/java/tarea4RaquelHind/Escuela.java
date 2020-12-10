08ea8f038aaba60db965e700d80ba8eea921db4b
raquelmvz <raquel.mvz98@gmail.com>
2020-12-10 18:37:40 +0100

    Añade getters y setters

--- src/main/java/tarea4RaquelHind/Escuela.java
+++ src/main/java/tarea4RaquelHind/Escuela.java
@@ -14,12 +14,34 @@ public class Escuela {
     private String nombre;
     private String direccion;
 
+    //Constructor por defecto
     public Escuela() {
        
     }
 
+    //Constructor parametrizado
     public Escuela(String nombre, String direccion) {
         this.nombre = nombre;
         this.direccion = direccion;
     }
+
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    //Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
 }
